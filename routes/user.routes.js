c1f4732e961ed8909b012d373ba6466c8c0836f9
akash wakade <mr.akashwakade@gmail.com>
2023-08-13 09:42:49 +0530

    added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,8 +15,9 @@ userRouter.post("/register",(req,res)=>{
             }else{
                 const user=new UserModel({name,email,pass:hash})
                 await user.save()
+                res.json({msg:"user has been registered",user:req.body})
             }
-        res.json({msg:"user has been registered",user:req.body})
+             
 
         })
     } catch (error) {
